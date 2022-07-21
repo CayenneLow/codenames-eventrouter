8433eddfcfc6cc9a1d2da68f6a26c9dcc3504a4b
Khye <lowkhyeean@gmail.com>
2022-07-21 23:51:04 +1000

    mongodb connection complete

--- internal/database/db.go
+++ internal/database/db.go
@@ -16,7 +16,9 @@ type Database interface {
 
 func Init(ctx context.Context, cfg config.Config) Database {
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(cfg.DbURI))
-	log.Fatalf("Unable to connect to DB: %s", err)
+	if err != nil {
+		log.Fatalf("Unable to connect to DB: %s", err)
+	}
 
 	collection := client.Database("test").Collection("delete_me")
 	collection.InsertOne(ctx, bson.D{{"_id", "T35T1"}, {"test", 123}})
